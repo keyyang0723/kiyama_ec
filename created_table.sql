2196f14894246009d3a636b57f85bc31f62ee761
木山優 <kiyamayuu@koyamayuunoMacBook-puro.local>
2018-03-15 16:22:11 +0900

    first commit

---
+++ created_table.sql
@@ -0,0 +1,44 @@
+-- Create syntax for TABLE 'admins'
+CREATE TABLE `admins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Create syntax for TABLE 'categories'
+CREATE TABLE `categories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Create syntax for TABLE 'orders'
+CREATE TABLE `orders` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) unsigned NOT NULL,
+  `price` int(11) NOT NULL,
+  `tax_rate` float(3,2) NOT NULL,
+  `customer_name` varchar(255) NOT NULL DEFAULT '',
+  `customer_address` varchar(255) NOT NULL DEFAULT '',
+  `customer_street` varchar(255) DEFAULT '',
+  `customer_zipcode` varchar(255) NOT NULL DEFAULT '',
+  `customer_tel` varchar(255) NOT NULL DEFAULT '',
+  `customer_email` varchar(255) NOT NULL DEFAULT '',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Create syntax for TABLE 'products'
+CREATE TABLE `products` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT '',
+  `description` text,
+  `id_displayed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+++ models/AdminRepository.php
@@ -0,0 +1,6 @@
+<?php
+
+class AdiminRepository extends DbRepository
+{
+	
+}
